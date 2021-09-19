3864c8a24b6f899b7ba6ab463eac8a25af2ba7d5
thiago <teagapk@gmail.com>
2021-09-18 21:16:40 -0300

    Criei um array com filmes e estilizei a página.

---
+++ index.js
@@ -0,0 +1,18 @@
+var listaFilmes = [
+  'https://assetsgn.nowonline.com.br/assets/p10679969_v_v12_ax.jpg',
+  'https://2.bp.blogspot.com/-MbdlL6o26rw/TisKalRbOZI/AAAAAAAAAP8/SgtOGrVU5JI/s1600/1245085520_harrypotter3poster05.jpg',
+  'http://tse2.mm.bing.net/th?id=OIP.K3iMlhsBVrXn9ug9EyAJLAAAAA&pid=15.1',
+  'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/67/58/20107766.jpg',
+  '',
+  'https://upload.wikimedia.org/wikipedia/pt/thumb/2/28/Resident_Evil.jpg/250px-Resident_Evil.jpg',
+  'https://br.web.img3.acsta.net/pictures/16/11/21/13/55/091105.jpg',
+  'https://media.fstatic.com/5efK3JaeKBY9XzSwRhso0pId7DQ=/290x478/smart/media/movies/covers/2011/11/7de299608aaa5702ada6f9e3cb16cd00.jpg',
+  'https://upload.wikimedia.org/wikipedia/pt/1/1e/The_Pursuit_of_Happyness.jpg',
+  'https://play-lh.googleusercontent.com/3MgN0iLwsGHBNr1PgjbMT7vSIbO1umICmUOToJjLzbcFs-I5_3shjcwdmZQU7nTAmtbhFw',
+  'https://upload.wikimedia.org/wikipedia/pt/thumb/6/6e/Lucifer_Temporada_2_Poster.jpg/230px-Lucifer_Temporada_2_Poster.jpg',
+  'https://rd1.com.br/wp-content/uploads/2013/08/Sobrenatural.jpg'
+]
+
+for (var indice = 0; indice < listaFilmes.length; indice++) {
+  document.write('<img src =' + listaFilmes[indice] + '>')
+}
